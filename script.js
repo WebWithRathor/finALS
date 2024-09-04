b870299be71239e0278e6052511371ec2417651b
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-05 00:02:36 +0530

    footer

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 
-const tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.frontPage',
-        start: 'top top',
-        end: '150%',
-        scrub: true,
-        markers: true,
-        // pin: true,
+// const tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: 'footer',
+//         start: 'top top',
+//         end: '150%',
+//         scrub: true,
+//         markers: true,
+//         // pin: true,
+//     }
+// })
+function jumpSmooth(){
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+jumpSmooth();
+
+function footerAnimation() {
+    function preventDefault(){
+        document.querySelectorAll('a').forEach(e => {
+            e.addEventListener('click', (event) => {
+                event.preventDefault();
+                console.log('clicked');
+            })
+            e.classList.add('hover')
+        })
     }
-})
-tl.to('.frontPage .center-box', {
-    left: '50%',
-    top: '50%',
-    width: '100vw',
-    height: '100vh',
-    duration: 2,
-}, 'a')
-    .to('.main-text', {
-        delay: 1,
-        color: 'white',
-    }, 'a')
-    .to('.main-text', {
-        delay: 1,
-        duration: 1,
-        left: '5rem',
-        top: '2rem',
-        fontSize: '2rem',
-    }, 'a')
+    // preventDefault();
+
+    const large_text = document.getElementById('idm-vton')
+    const letters = large_text.textContent.split('')
+    let clutter = "";
+    letters.forEach(e => {
+        clutter += `<span class="">${e}</span>`;
+    })
+    large_text.innerHTML = clutter;
+    gsap.set('#idm-vton span', { display: 'inline-block' })
+    gsap.from('#idm-vton span', {
+        transform: 'translateY(100%)',
+        stagger: 0.05,
+        duration : 1,
+        scrollTrigger: {
+            trigger: 'footer',
+            start: 'top 20%',
+            end: '10% top',
+            scrub: 2,
+            markers: true,
+        }
+    })
+}
+footerAnimation();
