a786391012329f524f4236f4960a0eff77919d16
WebWithAstha <lodhiastha85@gmail.com>
2024-09-04 23:59:49 +0530

    inropage

--- script.js
+++ script.js
@@ -30,9 +30,16 @@ function landingPageAnimation() {
             scale: 1,
             duration: .5
         }, '.2+a')
+        .to('#transition-circle',{
+            opacity:0,
+            duration:.1
+        },"qum")
         .to("#introContent", {
             clipPath: "polygon(0% 0%,100% 0%, 100% 100%, 0% 100% )"
-        })
+        },"qum")
+        // .from("#glitchImgs", {
+        //     y:200,
+        // },"qum")
 
 
 
@@ -93,3 +100,49 @@ function imgGlitchEffect() {
 
 }
 imgGlitchEffect()
+
+
+
+function minmap() {
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: '.minimap-wrapper',
+            start: 'top 0',
+            end: 'top -200%',
+            scrub: 1,
+            snap: [0.00, 0.33, 0.67, 1.00]
+        }
+    })
+
+    gsap.set('.minimap .images .img-box', {
+        y: '160%'
+    })
+
+    tl
+        .to('.virtual-photu', {
+            scale: 0.8,
+            x: '-50%',
+            opacity: 0
+        }, 'START')
+        .from('.minimap .images , .minimap .showimage , .minimap .details', {
+            x: '100vw',
+        }, 'START')
+
+        .to('.minimap  .imgBox', {
+            y: '-=105%'
+        }, 'a')
+        .to('.minimap .images .img-box', {
+            y: '-=115%'
+        }, 'a')
+        .to('.minimap .images .img-box', {
+            y: '-=115%'
+        }, 'b')
+        .to('.minimap  .imgBox', {
+            y: '-=105%'
+        }, 'b')
+
+}
+
+minmap();
