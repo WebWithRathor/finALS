4f45cd2392eaf1abeffa955a85c7fda790cb76bc
pratham meena <yashmeena274@gmail.com>
2024-09-04 20:51:06 +0530

    completed

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ const tl = gsap.timeline({
         scroller: 'body',  // Corrected 'scoller' to 'scroller'
         trigger: '.circlePage',
         start: 'top -1%',
-        end: 'top -100%',
+        end: 'top -150%',
         markers: true,  // Corrected 'marksers' to 'markers'
         scrub:1,
     }
 });
 
 tl.to('.circleDiv img', {
-    scale:2
+    scale:3
 },'a')
 .to('.circleDiv', {
     delay:.1,
-    height:'310vh',
-    width:'310vh',
+    height:'510vh',
+    width:'510vh',
 },'a')
 .to('.circleDiv', {
     right:'50%',
 },'a')
+.from('.leftText', {
+    width:0,
+},'a')
+.from('.leftText h1', {
+    left:'200%',
+},'a')
+.from('.rightText', {
+    width:0,
+},'a')
+.from('.rightText h1', {
+    left:'-300%',
+},'a')
+
+.to('.circleDiv', {
+    right:'80%',
+},'b')
+.to('.circleDiv img', {
+    scale:3.5
+},'b')
 
