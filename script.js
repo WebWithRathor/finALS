7742f436f2807314291620e6c6401250a31abf97
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-04 19:09:46 +0530

    .

--- script.js
+++ script.js
@@ -4,32 +4,35 @@ let arr = [
         "https://m.media-amazon.com/images/I/61xC6xsb7cL._SX679_.jpg"
     )
 ]
-const totalPercentage = 100;
-let currentPercentage = 0;
+function introAnimation(){
+    const totalPercentage = 100;
+    let currentPercentage = 0;
 
-const digitsElement = document.getElementById('digits');
+    const digitsElement = document.getElementById('digits');
 
-function updateLoading() {
-    if (currentPercentage <= totalPercentage) {
-        digitsElement.textContent = currentPercentage + '%';
-        currentPercentage++;
-    } else {
-        clearInterval(loadingInterval);
-        gsap.to('.loading-text',{
-            delay : 1,
-            // opacity : 0,
-            bottom : '-10%'
-        })
-        gsap.to('.intro-page',{
-            delay : 1.2,
-            color: 'black',
-            backgroundColor : 'white',
-        })
-        gsap.to('.center-text',{
-            delay : 1.2,
-            fontSize: "10vw",
-            ease: "bounce.out", 
-        })
+    function updateLoading() {
+        if (currentPercentage <= totalPercentage) {
+            digitsElement.textContent = currentPercentage + '%';
+            currentPercentage++;
+        } else {
+            clearInterval(loadingInterval);
+            gsap.to('.loading-text', {
+                delay: 1,
+                // opacity : 0,
+                bottom: '-10%'
+            })
+            gsap.to('.intro-page', {
+                delay: 1.2,
+                color: 'black',
+                backgroundColor: 'white',
+            })
+            gsap.to('.center-text', {
+                delay: 1.2,
+                fontSize: "10vw",
+                ease: "bounce.out",
+            })
+        }
     }
+    const loadingInterval = setInterval(updateLoading, 25);
 }
-const loadingInterval = setInterval(updateLoading, 25);
+// introAnimation();
